fc86dd65e52e4f8320037c4e3eeeecfc81b4a17e
Pat Hadley <pat@pathadley.net>
2016-06-06 15:46:29 +0100

    user availability visible on index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root to: "statics#welcome"
 
-  get '/users', to: "users#index"
+  get '/users', to: "users#index", as: "user_list"
 
   get '/users/:id', to: "users#show", as: "user_display"
 
