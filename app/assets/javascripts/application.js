140d33b3d73b6fa1e16e6a72cc5cde0f509a59b2
Pat Hadley <pat@pathadley.net>
2016-06-07 10:51:44 +0100

    help from Steve with nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,19 @@
 //= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
 
+
+// $(function() {
+//     if ( document.location.href.indexOf('users') > -1 ) {
+//         console.log ("I'm on the users list")
+//         $('#usersTab').addClass("active");
+//     } else if ( document.location.href.indexOf('profile') > -1 ) {
+//         console.log ("this is my profile page")
+//         $('#profileTab').addClass("active");
+//     } else if ( document.location.href.indexOf('sign_in') > -1 ) {
+//         $('#signinTab').addClass("active");
+//     } else if ( document.location.href.indexOf('sign_up') > -1 ) {
+//         $('#registerTab').addClass("active");
+//     } else
+//         $('#homeTab').addClass("active");
+//         console.log ("I'm on the homepage")
+// });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,8 @@ module ApplicationHelper
     def devise_mapping
       @devise_mapping ||= Devise.mappings[:user]
     end
+
+    def cp(path)
+      "active" if current_page?(path)
+    end
 end
