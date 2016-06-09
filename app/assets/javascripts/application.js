2041f8746dadbd47b572c81a65446ce15ffd9173
Pat Hadley <pat@pathadley.net>
2016-06-09 18:32:24 +0100

    minor tweaks and attempt at search partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,12 @@
 //= require ratyrate
 //= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
+
+
+$.ajax({
+    url: "/plants/index",
+    cache: false,
+    success: function(html){
+      $("#plant_searcher").append(html);
+    }
+});
